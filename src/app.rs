a35198190b955895abc2fa850f85e8b381201462
Kerwin Zheng <1747269691@qq.com>
2026-07-03 03:37:59 +0800

    feat(app): add App struct with state management
    Defines the main application state struct with fields for input directory,
    output file, FPS, messages, and quit status. Includes methods for
    initialization, increasing/decreasing FPS, and handling quit events.

---
+++ src/app.rs
@@ -0,0 +1,34 @@
+#[derive(Debug)]
+pub struct App {
+    pub input_dir: String,
+    pub output_file: String,
+    pub fps: u16,
+    pub messages: String,
+    pub should_quit: bool,
+}
+
+impl App {
+    pub fn new() -> Self {
+        Self {
+            input_dir: String::from("./frames"),
+            output_file: String::from("./output.gif"),
+            fps: 10,
+            message: String::from("Press enter ton generate GIF"),
+            should_quit: false,
+        }
+    }
+
+    pub fn increase_fps(&mut self) {
+        self.fps += 1;
+    }
+
+    pub fn decrease_fps(&mut self) {
+        if self.fps >= 1 {
+            self.fps -= 1;
+        }
+    }
+
+    pub fn quit(&mut self) {
+        self.should_quit = true;
+    }
+}
